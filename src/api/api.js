44c2caaba887098233be94330b6ab599211f7bcf
artem.votincev <artem.votincev@gmail.com>
2020-01-07 00:34:40 +0400

    Add google spreadsheet as a database

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,6 @@
 import * as axios from "axios";
+import csv from "csv-parser";
+var Readable = require('stream').Readable;
 
 const instanse = axios.create({
     baseURL: 'https://docs.google.com/'
@@ -8,10 +10,19 @@ export const postAPI = {
     getProducts() {
         return instanse.get('/spreadsheets/d/e/2PACX-1vTR3vyNCfIEo8rJm_5QeRCYFjppQlCMFdJ5zMCHp-LU-OdoS669zmZJhio_iXGSHKPKQVuE7mg6ALfb/pub?gid=0&single=true&output=csv')
             .then(response => {
-                return [                                                                              
-                    {id: 1, name: 'product1', description: 'description1', price: 100, currency: 'p'},
-                    {id: 2, name: 'product2', description: 'description2', price: 200, currency: 'p'}
-                ]
+                return new Promise((resolve, reject) =>{
+                    const results = [];
+                    const s = new Readable();
+                    var idx = 0;
+                    s._read = () => {}; // redundant? see update below
+                    s.push(response.data);
+                    s.push(null);
+                    s.pipe(csv())
+                     .on('data', (data) => results.push({...data, id: idx++}))
+                     .on('end', () => {
+                         resolve(results);
+                     });
+                });
             });
     }
 };
